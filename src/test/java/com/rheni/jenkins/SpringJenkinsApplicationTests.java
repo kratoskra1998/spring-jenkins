a09ea2f8e65f31ab04d05da2a4774e3fa8818039
kratoskra1998 <rheniusdevaraj98@gmail.com>
2024-05-04 22:41:09 +0530

    Third commit

--- src/test/java/com/rheni/jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/rheni/jenkins/SpringJenkinsApplicationTests.java
@@ -3,17 +3,20 @@ package com.rheni.jenkins;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 	
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 		logger.info("testcase executed..");
 		assertEquals(true, true);
 	}
